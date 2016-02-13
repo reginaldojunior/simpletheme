f212089c9124770385f51a2bebe3f1804c0a95a4
Reginaldo Junior <76regi@gmail.com>
2016-02-13 13:12:16 -0200

    first commit

---
+++ cache/81/81e453d05e072b9fce759160a11109f531cd2b18c449eae458755814c75a1c6f.php
@@ -0,0 +1,200 @@
+<?php
+
+/* layout.html */
+class __TwigTemplate_c826939f7c03768d3f5de858d089ae1db72794c4f7eab535afbbcdcda3f0d5d0 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<!DOCTYPE html>
+<html>
+<head>
+  \t<meta charset=\"utf-8\">
+  \t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+
+\t<title>Ganhe dinheiro com encurtadores de URL</title>
+    
+    <meta name=\"description\" content=\"Ganhe dinheiro encurtado URL's simplemeste fazendo seus twitters ou posts no facebook, além de tudo receba em dolar!\">
+\t
+\t<link rel=\"stylesheet\" type=\"text/css\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\">
+\t<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">
+
+\t<link rel=\"stylesheet\" type=\"text/css\" href=\"css/fullscreen/style.css\">
+</head>
+<body>
+
+\t<a href=\"#cd-nav\" class=\"cd-nav-trigger\">Menu 
+\t\t<span class=\"cd-nav-icon\"></span>
+
+\t\t<svg x=\"0px\" y=\"0px\" width=\"54px\" height=\"54px\" viewBox=\"0 0 54 54\">
+\t\t\t<circle fill=\"transparent\" stroke=\"#656e79\" stroke-width=\"1\" cx=\"27\" cy=\"27\" r=\"25\" stroke-dasharray=\"157 157\" stroke-dashoffset=\"157\"></circle>
+\t\t</svg>
+\t</a>
+\t<div id=\"cd-nav\" class=\"cd-nav\">
+\t\t<div class=\"cd-navigation-wrapper\">
+\t\t\t<div class=\"cd-half-block\">
+\t\t\t\t<nav>
+\t\t\t\t\t<ul class=\"cd-primary-nav list-unstyled\">
+\t\t\t\t\t\t<li><a href=\"/\" class=\"selected\">Ganhe Dinheiro</a></li>
+\t\t\t\t\t\t<li><a href=\"/shorten.html\">Encurtar URL</a></li>
+\t\t\t\t\t\t<li><a href=\"/jobs.html\">Trampos</a></li>
+\t\t\t\t\t\t<li><a href=\"/contact.html\">Fale Conosco</a></li>
+\t\t\t\t\t</ul>
+\t\t\t\t</nav>
+\t\t\t</div><!-- .cd-half-block -->
+\t\t\t
+\t\t\t<div class=\"cd-half-block\">
+\t\t\t\t<address>
+\t\t\t\t\t<ul class=\"cd-contact-info list-unstyled\">
+\t\t\t\t\t\t<li><a href=\"mailto:ganhedinheiro@ciawn.com.br\">ganhedinheiro@ciawn.com.br</a></li>
+\t\t\t\t\t</ul>
+\t\t\t\t</address>
+\t\t\t</div> <!-- .cd-half-block -->
+\t\t</div> <!-- .cd-navigation-wrapper -->
+\t</div>
+
+\t<div class=\"header\">
+\t\t<div class=\"background\">\t\t\t
+\t\t\t<div class=\"col-md-12\">
+\t\t\t\t<h1>Descubra como ganhar dinheiro com encurtador de URL's</h1>
+
+\t\t\t</div>
+\t\t\t<div class=\"col-md-6 col-md-offset-3\">
+\t\t\t\t<form>
+\t\t\t\t\t<input type=\"text\" class=\"form-control\" placeholder=\"Digite seu e-mail e receba o E-book\">
+\t\t\t\t\t<small>Nunca enviamos spam, achamos isso muito feio</small>
+\t\t\t\t</form>
+\t\t\t</div>
+\t\t\t<div class=\"col-md-6 col-md-offset-3\">
+\t\t\t\t<button type=\"button\" class=\"btn btn-success\">Quero saber como</button>
+\t\t\t</div>
+\t\t</div>
+\t</div>
+
+\t<script type=\"text/javascript\" src=\"js/fullscreen/jquery-2.1.1.js\"></script>
+\t<script type=\"text/javascript\" src=\"js/fullscreen/main.js\"></script>
+\t<script type=\"text/javascript\" src=\"js/fullscreen/modernizr.js\"></script>
+\t<script>
+\t  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+\t  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+\t  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+\t  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+\t  ga('create', 'UA-73730453-1', 'auto');
+\t  ga('send', 'pageview');
+
+\t</script>
+\t<script type=\"text/javascript\"> 
+\t    var adfly_id = 12638579; 
+\t    var popunder_frequency_delay = 0; 
+\t</script> 
+\t<script src=\"https://cdn.adf.ly/js/display.js\"></script> 
+</body>
+</html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "layout.html";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
+/* <!DOCTYPE html>*/
+/* <html>*/
+/* <head>*/
+/*   	<meta charset="utf-8">*/
+/*   	<meta name="viewport" content="width=device-width, initial-scale=1.0">*/
+/* */
+/* 	<title>Ganhe dinheiro com encurtadores de URL</title>*/
+/*     */
+/*     <meta name="description" content="Ganhe dinheiro encurtado URL's simplemeste fazendo seus twitters ou posts no facebook, além de tudo receba em dolar!">*/
+/* 	*/
+/* 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">*/
+/* 	<link rel="stylesheet" type="text/css" href="style.css">*/
+/* */
+/* 	<link rel="stylesheet" type="text/css" href="css/fullscreen/style.css">*/
+/* </head>*/
+/* <body>*/
+/* */
+/* 	<a href="#cd-nav" class="cd-nav-trigger">Menu */
+/* 		<span class="cd-nav-icon"></span>*/
+/* */
+/* 		<svg x="0px" y="0px" width="54px" height="54px" viewBox="0 0 54 54">*/
+/* 			<circle fill="transparent" stroke="#656e79" stroke-width="1" cx="27" cy="27" r="25" stroke-dasharray="157 157" stroke-dashoffset="157"></circle>*/
+/* 		</svg>*/
+/* 	</a>*/
+/* 	<div id="cd-nav" class="cd-nav">*/
+/* 		<div class="cd-navigation-wrapper">*/
+/* 			<div class="cd-half-block">*/
+/* 				<nav>*/
+/* 					<ul class="cd-primary-nav list-unstyled">*/
+/* 						<li><a href="/" class="selected">Ganhe Dinheiro</a></li>*/
+/* 						<li><a href="/shorten.html">Encurtar URL</a></li>*/
+/* 						<li><a href="/jobs.html">Trampos</a></li>*/
+/* 						<li><a href="/contact.html">Fale Conosco</a></li>*/
+/* 					</ul>*/
+/* 				</nav>*/
+/* 			</div><!-- .cd-half-block -->*/
+/* 			*/
+/* 			<div class="cd-half-block">*/
+/* 				<address>*/
+/* 					<ul class="cd-contact-info list-unstyled">*/
+/* 						<li><a href="mailto:ganhedinheiro@ciawn.com.br">ganhedinheiro@ciawn.com.br</a></li>*/
+/* 					</ul>*/
+/* 				</address>*/
+/* 			</div> <!-- .cd-half-block -->*/
+/* 		</div> <!-- .cd-navigation-wrapper -->*/
+/* 	</div>*/
+/* */
+/* 	<div class="header">*/
+/* 		<div class="background">			*/
+/* 			<div class="col-md-12">*/
+/* 				<h1>Descubra como ganhar dinheiro com encurtador de URL's</h1>*/
+/* */
+/* 			</div>*/
+/* 			<div class="col-md-6 col-md-offset-3">*/
+/* 				<form>*/
+/* 					<input type="text" class="form-control" placeholder="Digite seu e-mail e receba o E-book">*/
+/* 					<small>Nunca enviamos spam, achamos isso muito feio</small>*/
+/* 				</form>*/
+/* 			</div>*/
+/* 			<div class="col-md-6 col-md-offset-3">*/
+/* 				<button type="button" class="btn btn-success">Quero saber como</button>*/
+/* 			</div>*/
+/* 		</div>*/
+/* 	</div>*/
+/* */
+/* 	<script type="text/javascript" src="js/fullscreen/jquery-2.1.1.js"></script>*/
+/* 	<script type="text/javascript" src="js/fullscreen/main.js"></script>*/
+/* 	<script type="text/javascript" src="js/fullscreen/modernizr.js"></script>*/
+/* 	<script>*/
+/* 	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){*/
+/* 	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),*/
+/* 	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)*/
+/* 	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');*/
+/* */
+/* 	  ga('create', 'UA-73730453-1', 'auto');*/
+/* 	  ga('send', 'pageview');*/
+/* */
+/* 	</script>*/
+/* 	<script type="text/javascript"> */
+/* 	    var adfly_id = 12638579; */
+/* 	    var popunder_frequency_delay = 0; */
+/* 	</script> */
+/* 	<script src="https://cdn.adf.ly/js/display.js"></script> */
+/* </body>*/
+/* </html>*/
